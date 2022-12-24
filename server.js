26ce07990236fb5024339b0d4258d2fe3dff2cf0
JrGustavo <gus@soypetal.com>
2022-12-24 14:37:27 -0500

    "port upgrade"

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const app = express();
 app.get('/',(req, res) => res.send ('Hello world with express!'))
 
 
-app.listen(3000);
-console.log('Server on port 3000')
+app.listen(8080);
+console.log('Server on port 8080')
 
