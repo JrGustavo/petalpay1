2454c6cb7c3b47646774ec7c2657b1f0e0b65877
JrGustavo <gus@soypetal.com>
2022-12-24 20:50:24 -0500

    "probando path + express"

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
+import path from "path";
 
+const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => res.send ('Hello world with express!'))
 
 
+
 app.listen(8080);
 console.log('Server on port 8080')
 
+//Routes route
+app.get('/', (request, response) =>{
+    response.sendFile(path.resolve(__dirname, 'index.html'))
+})
